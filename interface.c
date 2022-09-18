cae059830ee1388ea1f1f74bac8decaf7368b8c1
Iskandar Umidov <30881757+iskandarumidov@users.noreply.github.com>
2022-09-18 23:08:35 +0000

    comments

--- interface.c
+++ interface.c
@@ -9,28 +9,28 @@ char *getId(char *inp) {
 
 int main(void)
 {
-  int toChild[2];
-  int toParent[2];
-  int err;
-  int childPid;
-  int status;
-  err = pipe(toChild);
+  int toChild[2];          // Pipe to child
+  int toParent[2];         // Pipe to parent
+  int err;                 // Will use for error checking
+  int childPid;            // Process ID of server
+  int status;              // Server exit status
+  err = pipe(toChild);     // Open toChild pipe file descriptor
   if (err == -1) {
     printf ("Error on pipe creation: %d\n", errno);
-    exit (1);
+    exit(EXIT_FAILURE);
   }
-  err = pipe(toParent);
+  err = pipe(toParent);    // Open toParent pipe file descriptor
   if (err == -1) {
     printf ("Error on pipe creation: %d\n", errno);
-    exit (1);
+    exit (EXIT_FAILURE);
   }
 
-  childPid = fork();
+  childPid = fork();       // Fork to get new process
   if (childPid == -1) {
     printf ("parent: fork failed, errno = %d\n", errno);
     exit (1);
-  } else if ( childPid == 0 ) { // IN CHILD NOW
-    if (childPid == 0) {
+  } else if ( childPid == 0 ) { // In child now if PID == 0
+    if (childPid == 0) {        // TODO - don't need the IF?
       close (toChild[1]);
       close (toParent[0]);
     }
@@ -38,13 +38,14 @@ int main(void)
     sprintf(char_fd_to_child, "%d", toChild[0]);
     char char_fd_to_parent[10];
     sprintf(char_fd_to_parent, "%d", toParent[1]);
-    
+
+    // Now run the Server.  First argument is program name, then pipe file descriptors
     err = execl ("./server", "server", char_fd_to_child, char_fd_to_parent, (char *)NULL);
     if ( err == -1 ) {
       printf ("parent: execl failed, errno = %d\n", errno);
-      exit (2);
+      exit(EXIT_FAILURE);
     }
-    exit(0);
+    exit(EXIT_SUCCESS);
   }
   printf ("parent: child created with pid = %d\n", childPid);
   close (toChild[0]);
@@ -72,18 +73,6 @@ int main(void)
   printf("PARENT FROM BYTES: %d\n", n);*/
 
   
-/*
-  if ( waitpid(childPid, &status, 0) == -1 ) {
-    perror("waitpid failed");
-    return EXIT_FAILURE;
-  }
-
-  if ( WIFEXITED(status) ) {
-    const int es = WEXITSTATUS(status);
-    printf("child exit status was %d\n", es);
-  }*/
-
-  
 
 
 
@@ -103,7 +92,7 @@ int main(void)
     n = read(toParent[0], &buf, 10000);
     if (n < 0) {
       printf("pipe read failed, errno = %d\n", errno);
-      exit(1);
+      exit(EXIT_FAILURE);
     }
     buf[n] = '\0';
     fflush(stdout);
@@ -113,11 +102,7 @@ int main(void)
     if (!strncmp(inp, "exit", strlen("exit"))){
       break;
     }
-
-
   }
-
-
     if ( waitpid(childPid, &status, 0) == -1 ) {
       perror("waitpid failed");
       return EXIT_FAILURE;
